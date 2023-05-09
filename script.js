ca64ccdf7348118eb68af4d595fa7c24f0af9fe9
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-05-09 13:40:20 +0100

    new game button resets the game

--- script.js
+++ script.js
@@ -14,17 +14,36 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add("hidden");
 
-// Scores
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Initializing variables
+let scores, currentScore, activePlayer, playing;
 
 // Functions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  btnNew.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+// Calling function to initialize starting conditions
+init();
+
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -63,7 +82,7 @@ btnHold.addEventListener("click", () => {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish game
       playing = false;
       diceEl.classList.add("hidden");
@@ -80,3 +99,5 @@ btnHold.addEventListener("click", () => {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
